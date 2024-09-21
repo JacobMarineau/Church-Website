957bf705382f2b20f5ed3c3949c6a5e3c7935bf6
JacobMarineau <jacobmarineaump@gmail.com>
2024-09-21 11:45:17 -0500

    front update

--- frontend/src/pages/ChurchCalendar.jsx
+++ frontend/src/pages/ChurchCalendar.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css'; 
+
+const localizer = momentLocalizer(moment);
+
+
+const initialEvents = [
+  {
+    id: 1,
+    title: 'Sunday Service',
+    start: new Date(2024, 8, 24, 10, 0), 
+    end: new Date(2024, 8, 24, 12, 0),
+  },
+  {
+    id: 2,
+    title: 'Wednesday Evening Bible Study',
+    start: new Date(2024, 8, 27, 19, 0),
+    end: new Date(2024, 8, 27, 21, 0),
+  },
+  {
+    id: 3,
+    title: 'Annual Church Picnic',
+    start: new Date(2024, 8, 30, 12, 0),
+    end: new Date(2024, 8, 30, 14, 0),
+  },
+];
 
 const ChurchCalendar = () => {
+  const [events, setEvents] = useState(initialEvents);
+
   return (
     <div className="container mt-5">
-      <h1>Church Calendar</h1>
-      <p>Check out the upcoming services and events below:</p>
-      <ul>
-        <li>Sunday Service - September 24th, 10 AM</li>
-        <li>Bible Study - September 27th, 7 PM</li>
-        <li>Church Picnic - October 1st, 12 PM</li>
-        <li>Special Event: Guest Speaker - October 5th, 6 PM</li>
-      </ul>
+      <h1 className="text-center">Church Calendar</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 };
